a07639d9624bea4085beb73336fb0f5df2fc3f9f
Corwin <cgjmacmi@edu.uwaterloo.ca>
2022-11-10 15:28:50 -0500

    Python scripts to load and decompress NAC EDR images

---
+++ decompand.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# this script contains functions and conversion tables for companding and decompanding the NAC EDR images
+
+# from LROC:
+# In flight, the 12-bit raw DN values are companded to 8-bit values sent back to Earth and written in the EDRs. 
+# This companding uses one of six on-board schemes shown in Fig. 16. To enable fast pixel readout, each bit-compression 
+# scheme consists of a piecewise linear function composed of up to 5 segments. Most images use companding table 0. 
+# Table 1 (8-bit equals 12-bit) is used for darks. Table 3 samples low DN more densely, and it is used frequently 
+# for dim images. On the ground, the first step in the image processing pipeline is to take the 8-bit values in 
+# the EDRs and calculate a floating point value that approximates the original 12-bit integer value. The approximation
+# calculated is the unweighted mean of all the 12-bit raw DN values that would be companded the 8-bit value found 
+# in the EDR.
+
+# we convert using the companding psudeocode in LROCSIS.PDF and the decompanding script here (converted to python):
+# https://github.com/USGS-Astrogeology/ISIS3/blob/dev/isis/src/lro/apps/lronac2isis/main.cpp
+
+
+import numpy as np
+
+
+# DEFINE COMPANDING TABLES (see LROCSIS.PDF)
+
+_bterms = \
+{0: [0, 8, 25, 59, 128],
+ 1: [0, 0, 0, 0, 0],
+ 2: [0, 0, 0, 0, 0],
+ 3: [0, 16, 69, 103, 128],
+ 4: [0, 0, 0, 65, 128],
+ 5: [0, 0, 14, 65, 128]}
+
+_xterms = \
+{0: [0, 32, 136, 543, 2207],
+ 1: [511, 0, 0, 0, 0],
+ 2: [0, 0, 0, 0, 4095],
+ 3: [0, 64, 424, 536, 800],
+ 4: [0, 0, 0, 1040, 2000],
+ 5: [0, 0, 112, 816, 2000]}
+
+_mterms = \
+{0: [0.5, 0.25, 0.125, 0.0625, 0.03125],
+ 1: [0.5, 0.25, 0.125, 0.0625, 0.03125],
+ 2: [0.5, 0.25, 0.125, 0.0625, 0.03125],
+ 3: [0.5, 0.25, 0.125, 0.0625, 0.03125],
+ 4: [0.5, 0.25, 0.125, 0.0625, 0.03125],
+ 5: [0.5, 0.25, 0.125, 0.0625, 0.03125]}
+
+_MAX_INPUT_VALUE = 4095
+
+def _get_decompand_maps():
+    """"Pre-compute decompanding conversion maps for fast decompanding.
+    This is a python implementation of this ISIS script:
+    # https://github.com/USGS-Astrogeology/ISIS3/blob/dev/isis/src/lro/apps/lronac2isis/main.cpp
+    """
+    
+    base = np.arange(256, dtype=np.uint8)
+    decompand_maps = {}
+    
+    for table in range(6):
+        
+        decompand_maps[table] = np.zeros(256, dtype=np.uint16)
+        
+        # get companding table
+        xterm = _xterms[table]# the y-intercept of the linear function
+        bterm = _bterms[table]# the inflection point on the x-axis (12-bit axis)
+        mterm = _mterms[table]# the gradient?
+        
+        for i,p in enumerate(base):
+            
+            # if pixin < xterm0, then it is in "segment 0"
+            if p < xterm[0]:
+                decompand_maps[table][i] = p
+            
+            # otherwise, it is in segments 1 to 5
+            else:
+                segment = 1
+                while segment < len(xterm) and (p - bterm[segment - 1]) / mterm[segment - 1] >= xterm[segment]:
+                    segment += 1
+            
+                # Compute the upper and lower bin values
+                upper = (p + 1 - bterm[segment - 1]) / mterm[segment - 1] - 1
+                lower = (p - bterm[segment - 1]) / mterm[segment - 1]
+                
+                # Check if the bin is on the upper boundary of the last segment
+                if(upper > _MAX_INPUT_VALUE):
+                    upper = _MAX_INPUT_VALUE
+                elif segment < len(xterm) and upper >= xterm[segment]:
+                    if int(bterm[segment] + mterm[segment]*upper) != p:
+                        upper = xterm[segment] - 1
+                    
+                # Check if it is on the lower boundary of a segment
+                if lower < xterm[segment-1]:
+                    lower = xterm[segment-1]
+                  
+                # Output the middle bin value
+                decompand_maps[table][i] = (upper + lower) / 2.0# note casts as integer value if array is uint
+            
+    return decompand_maps
+    
+decompand_maps = _get_decompand_maps()
+
+def _get_compand_maps():
+    """"Pre-compute companding conversion maps for fast companding.
+    This is a python implementation of the psuedocode in LROCSIS.PDF.
+    """
+    
+    base = np.arange(4096, dtype=np.uint16)
+    compand_maps = {}
+    
+    for table in range(6):
+        
+        compand_maps[table] = np.zeros(4096, dtype=np.uint8)
+    
+        # get companding table
+        xterm = _xterms[table]# the y-intercept of the linear function
+        bterm = _bterms[table]# the inflection point on the x-axis (12-bit axis)
+        
+        for i,p in enumerate(base):
+            
+            if p < xterm[0]:
+                pout = p%256
+            elif p < xterm[1]: 
+                pout = p/2+bterm[0]
+            elif p < xterm[2]: 
+                pout = p/4+bterm[1]
+            elif p < xterm[3]: 
+                pout = p/8+bterm[2]
+            elif p < xterm[4]: 
+                pout = p/16+bterm[3]
+            else:
+                pout = p/32+bterm[4]
+            
+            # Output the middle bin value
+            compand_maps[table][i] = pout# note casts as integer value if array is uint
+            
+    return compand_maps
+
+compand_maps = _get_compand_maps()
+
+
+    
+def decompand(image, table):
+    "Decompand an 8-bit NAC image to 12-bits, cast as 16-bit integers"
+    
+    if image.dtype != np.uint8: raise Exception("ERROR: image is not np.uint8! (%s)"%(image.dtype))
+    if image.ndim != 2: raise Exception("ERROR: image dimension is not 2! (%s)"%(image.shape,))
+    if table not in np.arange(6): raise Exception("ERROR: table not in 0-5! (%s)"%(table))
+    
+    # copy is probably not needed, but do it for safety
+    # https://scipy-cookbook.readthedocs.io/items/ViewsVsCopies.html#I-think-I-understand-what-a-view-is,-but-why-fancy-indexing-is-not-returning-a-view?
+    image = decompand_maps[table][image].copy()# map
+    assert image.dtype == np.uint16
+    return image
+
+def compand(image, table):
+    "Compand a 12-bit NAC image, cast as 16-bit integers, to 8-bit"
+    
+    if image.dtype != np.uint16: raise Exception("ERROR: image is not np.uint16! (%s)"%(image.dtype))
+    if image.ndim != 2: raise Exception("ERROR: image dimension is not 2! (%s)"%(image.shape,))
+    if table not in np.arange(6): raise Exception("ERROR: table not in 0-5! (%s)"%(table))
+    
+    # copy is probably not needed, but do it for safety
+    # https://scipy-cookbook.readthedocs.io/items/ViewsVsCopies.html#I-think-I-understand-what-a-view-is,-but-why-fancy-indexing-is-not-returning-a-view?
+    image = compand_maps[table][image].copy()# map
+    assert image.dtype == np.uint8
+    return image
+
+if __name__ == "__main__":
+    
+    import time
+    import matplotlib.pyplot as plt
+    
+    
+    # plot companding / decompanding maps
+    plt.figure(figsize=(10,10))
+    
+    base = np.arange(4096, dtype=np.uint16)
+    for table in range(6):
+        plt.plot(base, compand_maps[table], lw=3, label=table)
+    
+    base = np.arange(256, dtype=np.uint8)
+    for table in range(6):
+        plt.plot(decompand_maps[table], base, color="k", label=table)
+    
+    plt.legend()
+    plt.show()
+    
+    
+    # test using realistic image size (250 MB)
+    t = 3
+    
+    np.random.seed(123)
+    image = np.random.randint(0, 4096, size=(52224, 5064), dtype=np.uint16)
+        
+    start = time.time()
+    image2 = compand(image, t)
+    print("%.2f s"%(time.time()-start))
+    
+    start = time.time()
+    image3 = decompand(image2, t)
+    print("%.2f s"%(time.time()-start))
+    
+    plt.figure()
+    plt.scatter(image.flatten()[:1000], image2.flatten()[:1000], s=2)
+    
+    plt.figure()
+    plt.scatter(image3.flatten()[:1000], image2.flatten()[:1000], s=2)
+    
+    plt.figure()
+    plt.scatter(np.arange(1000), image3.flatten()[:1000].astype(float)-image.flatten()[:1000].astype(float), s=1)
+    
+    plt.show()
+
+    
---
+++ planetaryimageEDR.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This script extends planetaryimage for loading EDR images
+
+# It:
+# 1) fixes the decoding bug when loading EDR images
+
+# 2) adds support for decompressing the 8-bit values to 12-bit values (cast as 16-bit floats which numpy can handle)
+
+# 3) checks md5 sums for file integrity
+
+# 4) improves image updating and saving capability
+
+
+import os
+import hashlib
+
+import numpy as np
+
+import pvl
+from planetaryimage import PDS3Image
+import decompand
+
+
+
+class PDS3ImageEDR(PDS3Image):
+    """EDR image class, extends PDS3Image class by fixing EDR decoding bug, adding support for decompressing
+    8-bit values to 12-bit instrument DN values and checking md5 file integrity.
+    """
+    
+    # this fixes the decoding bug by overriding the class SAMPLE_TYPE and DTYPES attributes
+    # i.e. the EDR image (self.data) is loaded correctly as 8-bit unsigned integers
+    
+    SAMPLE_TYPES = {
+        'LSB_INTEGER': '<u',# nb PSD3Image figures out these are 8-bit using int(self.label['IMAGE']['SAMPLE_BITS'] / 8)
+    }
+
+    DTYPES = {
+        '<u': 'LSB_INTEGER',
+        '<f': 'PC_REAL',# allows images to be updated to floating points
+    }
+    
+    
+    # override the class initialisation to add md5 check and decompanding
+    def __init__(self, *args, **kwargs):
+        """
+        Create an Image object.
+        """
+        super().__init__(*args, **kwargs)
+        
+        # md5 check
+        assert hashlib.md5(self.data.tobytes()).hexdigest() == self.label["IMAGE"]["MD5_CHECKSUM"]
+        
+        # product type check
+        assert self.label["PRODUCT_TYPE"] == "EDR"
+        assert self.label["PRODUCT_ID"].endswith("E")
+        
+        # get image
+        image = self.data.squeeze()
+        
+        # The code below backs off the LROC companding tables, converting the
+        # 8-bit unsigned integers to the original 12-bit unsigned integers
+        # recorded on the instrument, cast as 16-bit unsigned integers 
+        # (because numpy does not support 12-bit integers).
+        table = self.label["LRO:COMPAND_CODE"]# integer table number
+        assert self.label["LRO:BTERM"] == decompand._bterms[table]# check terms are as expected
+        assert self.label["LRO:XTERM"] == decompand._xterms[table]
+        assert self.label["LRO:MTERM"] == decompand._mterms[table]
+        self._image = decompand.decompand(image, table)# decompanded image
+        
+        # get image identifier (using our naming scheme)
+        self.PRODUCT_ID = self.label["PRODUCT_ID"].replace("E","C")
+        assert self.PRODUCT_ID in os.path.basename(self.filename)
+        
+        
+    # override image property (make it static)
+    @property
+    def image(self):
+        return self._image
+    
+    
+    # new method for updating image array in place
+    def update_image(self, im):
+        "Update the image array with some new data"
+        
+        if im.shape != self.image.shape: raise Exception("ERROR: input image is not the same shape as current image! (%s) (%s)"%(im.shape, self.image.shape))
+        
+        # update underlying data, force to be LSB
+        self.data = np.expand_dims(im.copy(), 0).astype("<%s%i"%(im.dtype.kind, im.dtype.itemsize))# add band dimension
+        self._image = self.data.squeeze()
+        
+        # update label
+        self.label['IMAGE']['LINES'] = self.data.shape[1]
+        self.label['IMAGE']['LINE_SAMPLES'] = self.data.shape[2]
+        self.label["IMAGE"]["MD5_CHECKSUM"] = hashlib.md5(self.data.tobytes()).hexdigest()
+        self.label['IMAGE']['SAMPLE_BITS'] = self.data.itemsize * 8
+        self.label['IMAGE']['SAMPLE_TYPE'] = self.DTYPES["<" + self.dtype.kind]
+        
+        
+    # override save: remove overwriting, instantiate PDSLabelEncoder and use encoder=encoder argument, clean up padding
+    def save(self, filepath):
+        """Save PDS3Image object as PDS3 file.
+        """
+        
+        # TODO: update FILE_RECORDS? (might be ok because we set LINES, LINE_SAMPLES above..)
+        
+        encoder = PDSLabelEncoderEDR()
+        
+        # get image pointer and length of label in bytes
+        LABEL_RECORDS = self.label['^IMAGE'] = 0
+        while self.label['^IMAGE'] != LABEL_RECORDS+1:
+            
+            # update image pointer
+            self.label['^IMAGE'] = LABEL_RECORDS+1
+            
+            # get length of label in bytes
+            label_nbytes = len(pvl.dumps(self.label, encoder=encoder).encode("ascii"))
+            LABEL_RECORDS = int(np.ceil(label_nbytes / self.label['RECORD_BYTES']))
+            
+            # update label records
+            self.label["LABEL_RECORDS"] = LABEL_RECORDS
+            
+        # get padding required to make label fixed record length
+        diff =  LABEL_RECORDS*self.label['RECORD_BYTES'] - label_nbytes
+        
+        # write label
+        pvl.dump(self.label, filepath, encoder=encoder)
+        
+        with open(filepath, 'a') as stream:
+
+            # write padding
+            for i in range(0, diff):
+                stream.write(" ")
+            
+            # write array as bytes
+            self.data.tofile(stream)
+
+
+
+class PDSLabelEncoderEDR(pvl.encoder.PDSLabelEncoder):
+    """EDR PDS label encoder class, overrides time formatting (times without 'Z' character added)
+    and 30 character limit for keys"""
+    
+    # override for skipping value error when keys are larger than 30
+    def encode_assignment(self, key, value, level=0, key_len=None):
+        
+        if key_len is None:
+            key_len = len(key)
+
+        if len(key) > 30:
+            pass
+
+        ident = ''
+        if((key.startswith('^') and self.is_assignment_statement(key[1:]))
+           or self.is_assignment_statement(key)):
+            ident = key.upper()
+        else:
+            raise ValueError(f'The keyword "{key}" is not a valid ODL '
+                             'Identifier.')
+
+        s = ''
+        s += '{} = '.format(ident.ljust(key_len))
+        s += self.encode_value(value)
+
+        if self.end_delimiter:
+            s += self.grammar.delimiters[0]
+
+        return self.format(s, level)
+    
+    # override for removing "Z" in datetime strings
+    def encode_time(self, value):# this is a static method in PVLEncoder
+        return pvl.encoder.PVLEncoder.encode_time(value)
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    
+    import matplotlib.pyplot as plt
+    
+    
+    ## test reading
+    
+    
+    I = PDS3Image
+    print(I.SAMPLE_TYPES)
+    
+    I = PDS3Image.open("../lroc/data/edr/M107725040LC")
+    print(I.data.dtype)
+    print(I.data.min(), I.data.max())
+    print(I.image.dtype)
+    print(I.image.min(), I.image.max())
+    
+    # plot I.image
+    plt.figure(figsize=(20,3))
+    plt.imshow(I.image.astype(float), cmap="gray", vmin=20, vmax=60)
+    plt.colorbar()
+    plt.figure(figsize=(20,3))
+    for i in range(5): plt.plot(I.image[i,:])
+    
+    
+    print()
+    I = PDS3ImageEDR
+    print(I.SAMPLE_TYPES)
+    
+    I = PDS3ImageEDR.open("../lroc/data/edr/M107725040LC")
+    print(I.data.dtype)
+    print(I.data.min(), I.data.max())
+    print(I.image.dtype)
+    print(I.image.min(), I.image.max())
+    
+    # plot I.image
+    plt.figure(figsize=(20,3))
+    plt.imshow(I.image.astype(float), cmap="gray", vmin=20, vmax=60)
+    plt.colorbar()
+    plt.figure(figsize=(20,3))
+    for i in range(5): plt.plot(I.image[i,:])
+    
+    plt.show()
+    
+    
+    ## test saving
+    
+    
+    # straight save
+    I = PDS3ImageEDR.open("../lroc/data/edr/M107725040LC")
+
+    plt.figure(figsize=(20,3))
+    plt.imshow(I.image, cmap="gray", vmin=20, vmax=60)
+    plt.colorbar()
+    
+    #I.data = I.data.astype(np.float64)
+    I.save("M107725040LC")
+    I = PDS3ImageEDR.open("M107725040LC")
+    
+    print(I.label)
+    print(I.data.dtype)
+    print(I.data.shape)
+    
+    plt.figure(figsize=(20,3))
+    plt.imshow(I.image, cmap="gray", vmin=20, vmax=60)
+    plt.colorbar()
+    
+    # update and save
+    I = PDS3ImageEDR.open("../lroc/data/edr/M107725040LC")
+    a1 = I.image.copy()
+    
+    plt.figure(figsize=(20,3))
+    plt.imshow(I.image, cmap="gray", vmin=20, vmax=60)
+    plt.colorbar()
+    
+    I.update_image(I.image.astype(np.float32))
+    I.save("test")
+    I = PDS3Image.open("test")
+    a2 = I.image.copy()
+    
+    print(I.label)
+    print(I.data.dtype)
+    print(I.data.shape)
+    print(np.allclose(a1.astype(int), a2.astype(int)))
+    
+    plt.figure(figsize=(20,3))
+    plt.imshow(I.image, cmap="gray", vmin=20, vmax=60)
+    plt.colorbar()
