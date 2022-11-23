0e2f9117313bb470d68564b738434b066aa4b1ce
Corwin <cgjmacmi@edu.uwaterloo.ca>
2022-11-22 20:39:47 -0500

    Only download summed images, print estimated time for downloading the current csv

--- download_image.py
+++ download_image.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import os
 from pathlib import Path
+import time
 
 
 def download_image(file_spec, filepath):
@@ -36,16 +37,25 @@ def download_image_list(img_csv, download_path, continuation_index=0):
     img_list = pd.read_csv(img_csv)
     file_specs = img_list[['FILE_SPECIFICATION_NAME']].to_numpy()
     list_size = file_specs.size
+    sum_time = 0
     for i, file_spec in enumerate(file_specs[continuation_index:]):
+        start = time.time()
         download_image(file_spec[0], download_path)
-        print('File {}, {} / {}'.format(img_csv + file_spec[0], i + continuation_index, list_size))
+        end = time.time()
+        sum_time += end - start
+        hours_left = round((sum_time/(i+1) * list_size - (i + continuation_index) * sum_time/(i+1))/3600, 2)
+        print('File {}, {} / {}, estimated hours left: {}'.format(img_csv + file_spec[0], i + continuation_index,
+                                                                  list_size, hours_left))
 
 
 if __name__ == '__main__':
     csv_dir = 'edr/'
     download_dir = '/media/panlab/EXTERNALHDD/'
-    for csv_file in [os.listdir(csv_dir)[1]]:
-        download_image_list(csv_dir + csv_file, download_dir + csv_file[:-4] + '/')
+    for csv_file in os.listdir(csv_dir):
+        if csv_file == 'bright_normal.csv' or csv_file == 'dark_normal.csv':
+            pass
+        else:
+            download_image_list(csv_dir + csv_file, download_dir + csv_file[:-4] + '/', 34)
        
 
 
