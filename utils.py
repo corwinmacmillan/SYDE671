31e389c19efa54bcb20c70910e562d748814f8b5
jh3chu <jon25331@hotmail.com>
2022-11-25 23:04:54 -0500

    initial split_destripe commit

--- utils.py
+++ utils.py
@@ -3,14 +3,73 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-import albumentations 
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-from torch.utils.data import(
-    Dataset,
-    Dataloader,
-    
+from torch.utils.data import (
+    DataLoader,
+    Dataset,    
 ) 
 
 from torchvision.io import read_image
+
+from torchvision.transforms import (
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    RandomRotation,
+)
+
+def split_destripe(
+    destripe_data,
+    destripe_path,
+    split_size = 0.2
+):
+    '''
+    :params:
+        destripe_data: csv of destripe inputs and labels from noisy_img.py
+        destripe_path: path to destripe train and val folders
+        split_size: validation dataset size for train_test_split()
+    '''
+        
+    destripe_path_train = os.path.join(destripe_path, 'train')
+    destripe_path_val = os.path.jon(destripe_path_train, 'val')
+
+    # Check for train/val folders
+    if not os.path.exists(destripe_path_train):
+        os.makedirs(destripe_path_train)
+    if not os.path.exists(destripe_path_val):
+        os.makedirs(destripe_path_val)
+
+    # Read destripe data
+    df = pd.read_csv(destripe_data)
+
+    # Split data into inputs and labels
+    y = df[['Filename', 'Pixel_line']]
+    X = df.drop('Pixel_line', axis=1)
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=split_size)
+
+    # Save training and validation data
+    X_train.to_csv(os.path.join(destripe_path_train, 'train_inputs'))
+    y_train.to_csv(os.path.join(destripe_path_train, 'train_labels'))
+
+    X_val.to_csv(os.path.join(destripe_path_val, 'val_inputs'))
+    y_val.to_csv(os.path.join(destripe_path_val, 'val_labels'))
+    
+
+def destripe_get_loaders(
+    train_files,
+    val_files,
+    batch_size,
+    shuffle,
+):
+    pass
+
+def photon_get_loaders(
+    train_files,
+    val_files,
+    batch_size,
+    shuffle,
+):
+    pass
 
